496863f292aca8cd55955b34cd7d62d6e048e60f
Isaiah <issyzac.iz@gmail.com>
2019-10-28 10:52:03 +0300

    feat: added site loader

---
+++ src/components/indicator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function LoadIndicator() {
+    return (
+        <div className="preloader">
+            <div className="la-ball-scale-multiple la-2x">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+export default LoadIndicator;
