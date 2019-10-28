8cbed37a7cabcaeb2aa93e77422436ec3c2db894
Isaiah <issyzac.iz@gmail.com>
2019-10-28 11:06:45 +0300

    feat: banner component for the homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoadIndicator from './components/indicator';
+import Header from './components/header';
+import Banner from './components/banner';
 
 class App extends Component {
   render() {
     return (
       <>
-        <LoadIndicator />
+        {/* <LoadIndicator /> */}
+        <Header />
+        <Banner />
       </>
     )
   }
