4213172d49998f8d8ca184f754a120d5cf6c068c
Isaiah <issyzac.iz@gmail.com>
2019-10-29 20:54:15 +0300

    chore: code cleanup

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 function Header() {
     return (
@@ -64,4 +64,4 @@ function Header() {
     )
 }
 
-export default Header;
+export default Header
