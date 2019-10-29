4c6ca7c1e652ff4fdc744f355c0e27b177eb4899
Isaiah <issyzac.iz@gmail.com>
2019-10-29 20:53:32 +0300

    chore: convert functional to class component

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,152 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-function Banner() {
-    return (
-        <section class="banner_01">
-            <div class="revSlider">
-                <ul>
-                    <li data-transition="boxfade" data-slotamount="7" data-masterspeed="1000" >
-                        <div class="tp-caption sfb"
-                            data-x="left"
-                            data-y="top"
-                            data-hoffset="0"
-                            data-voffset="-80"
-                            data-speed="1000"
-                            data-start="800"
-                            data-easing="easeInOutCirc">
-                            <div class="sliderImgs">
-                                <img src="images/slider/sl1.jpg" alt="" />
+class Banner extends Component {
+
+    render() {
+        return (
+            <section className="banner_01">
+                <div className="revSlider">
+                    <ul>
+                        <li data-transition="boxfade" data-slotamount="7" data-masterspeed="1000" >
+                            <div className="tp-caption sfb"
+                                data-x="left"
+                                data-y="top"
+                                data-hoffset="0"
+                                data-voffset="-80"
+                                data-speed="1000"
+                                data-start="800"
+                                data-easing="easeInOutCirc">
+                                <div className="sliderImgs">
+                                    <img src="images/slider/sl1.jpg" alt="" />
+                                </div>
                             </div>
-                        </div>
-                        <div class="tp-caption sfb"
-                            data-x="right"
-                            data-y="center"
-                            data-hoffset="0"
-                            data-voffset="0"
-                            data-speed="1000"
-                            data-start="800"
-                            data-easing="easeInQuad">
-                            <h1 class="sl_h raleway">FEEL THE TRENDY FASHION</h1>
-                        </div>
-                        <div class="tp-caption fade"
-                            data-x="right"
-                            data-y="center"
-                            data-hoffset="0"
-                            data-voffset="65"
-                            data-speed="1000"
-                            data-start="800"
-                            data-easing="easeInQuint">
-                            <h6 class="sl_p poppins">Get FLAT 30% Off For All New Arrivals</h6>
-                        </div>
-                        <div class="tp-caption sfb"
-                            data-x="right"
-                            data-y="center"
-                            data-hoffset="0"
-                            data-voffset="145"
-                            data-speed="1000"
-                            data-start="800"
-                            data-easing="easeOutBounce">
-                            <div class="slbutons">
-                                <a href="index.html#" class="vol_btn poppins">Shop Now<i class="flaticon-arrows-3"></i></a>
+                            <div className="tp-caption sfb"
+                                data-x="right"
+                                data-y="center"
+                                data-hoffset="0"
+                                data-voffset="0"
+                                data-speed="1000"
+                                data-start="800"
+                                data-easing="easeInQuad">
+                                <h1 className="sl_h raleway">FEEL THE TRENDY FASHION</h1>
                             </div>
-                        </div>
-                    </li>
-                    <li data-transition="boxfade" data-slotamount="7" data-masterspeed="1000" >
-                        <div class="tp-caption sft"
-                            data-x="right"
-                            data-y="top"
-                            data-hoffset="0"
-                            data-voffset="-80"
-                            data-speed="1000"
-                            data-start="800"
-                            data-easing="easeInOutCirc">
-                            <div class="sliderImgs">
-                                <img src="images/slider/sl2.jpg" alt="" />
+                            <div className="tp-caption fade"
+                                data-x="right"
+                                data-y="center"
+                                data-hoffset="0"
+                                data-voffset="65"
+                                data-speed="1000"
+                                data-start="800"
+                                data-easing="easeInQuint">
+                                <h6 className="sl_p poppins">Get FLAT 30% Off For All New Arrivals</h6>
                             </div>
-                        </div>
-                        <div class="tp-caption sfb"
-                            data-x="left"
-                            data-y="center"
-                            data-hoffset="0"
-                            data-voffset="0"
-                            data-speed="1000"
-                            data-start="800"
-                            data-easing="easeInQuad">
-                            <h1 class="sl_h raleway">GREAT FASHION WITH VOLTA</h1>
-                        </div>
-                        <div class="tp-caption fade"
-                            data-x="left"
-                            data-y="center"
-                            data-hoffset="0"
-                            data-voffset="65"
-                            data-speed="1000"
-                            data-start="800"
-                            data-easing="easeInQuint">
-                            <h6 class="sl_p poppins">Get FLAT 30% Off For All New Arrivals</h6>
-                        </div>
-                        <div class="tp-caption sfb"
-                            data-x="left"
-                            data-y="center"
-                            data-hoffset="0"
-                            data-voffset="145"
-                            data-speed="1000"
-                            data-start="800"
-                            data-easing="easeOutBounce">
-                            <div class="slbutons leftslBtn">
-                                <a href="index.html#" class="vol_btn poppins">Shop Now<i class="flaticon-arrows-3"></i></a>
+                            <div className="tp-caption sfb"
+                                data-x="right"
+                                data-y="center"
+                                data-hoffset="0"
+                                data-voffset="145"
+                                data-speed="1000"
+                                data-start="800"
+                                data-easing="easeOutBounce">
+                                <div className="slbutons">
+                                    <a href="index.html#" className="vol_btn poppins">Shop Now<i className="flaticon-arrows-3"></i></a>
+                                </div>
                             </div>
-                        </div>
-                    </li>
-                    <li data-transition="boxfade" data-slotamount="7" data-masterspeed="1000" >
-                        <div class="tp-caption sft"
-                            data-x="left"
-                            data-y="top"
-                            data-hoffset="0"
-                            data-voffset="-80"
-                            data-speed="1000"
-                            data-start="800"
-                            data-easing="easeInOutCirc">
-                            <div class="sliderImgs">
-                                <img src="images/slider/sl3.jpg" alt="" />
-                            </div>
-                        </div>
-                        <div class="tp-caption sfb"
-                            data-x="right"
-                            data-y="center"
-                            data-hoffset="0"
-                            data-voffset="0"
-                            data-speed="1000"
-                            data-start="800"
-                            data-easing="easeInQuad">
-                            <h1 class="sl_h raleway">VOLTA THE GLORY OF FASHION </h1>
-                        </div>
-                        <div class="tp-caption fade"
-                            data-x="right"
-                            data-y="center"
-                            data-hoffset="0"
-                            data-voffset="65"
-                            data-speed="1000"
-                            data-start="800"
-                            data-easing="easeInQuint">
-                            <h6 class="sl_p poppins">Get FLAT 30% Off For All New Arrivals</h6>
-                        </div>
-                        <div class="tp-caption sfb"
-                            data-x="right"
-                            data-y="center"
-                            data-hoffset="0"
-                            data-voffset="145"
-                            data-speed="1000"
-                            data-start="800"
-                            data-easing="easeOutBounce">
-                            <div class="slbutons">
-                                <a href="index.html#" class="vol_btn poppins">Shop Now<i class="flaticon-arrows-3"></i></a>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </section>
-    )
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        )
+    }
 }
 
-export default Banner;
+export default Banner
