487a64324a81a9ed67075c8a71bb97f856eaa864
Isaiah <issyzac.iz@gmail.com>
2019-10-30 10:43:36 +0300

    feat: created home about section component

---
+++ src/components/about.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function AboutSection() {
+    return (
+        <section className="comonSection">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="sectionTitle text-center">
+                            <h2>About Minimal Shop</h2>
+                            <div className="titleBars"></div>
+                            <p>
+                                The Love Boat soon will be making another run the love boat promises something for everyone one two three four five
+                                six seven eight Sclemeel schlemazel hasenfeffer incorporated.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-12 aboutCall">
+                        <div className="aboutMinimal">
+                            <div className="row">
+                                <div className="col-sm-6 wow fadeInUp" data-wow-duration="700ms" data-wow-delay="300ms">
+                                    <div className="aboutImg">
+                                        <img src="images/home2/about1.png" alt="" />
+                                    </div>
+                                </div>
+                                <div className="col-sm-6 wow fadeInUp" data-wow-duration="700ms" data-wow-delay="350ms">
+                                    <div className="aboutDesc">
+                                        <h4 className="poppins">MINIMALISM STORY</h4>
+                                        <p>
+                                            The Love Boat soon will be making another run the love boat
+                                            promises something for everyone one two three four five six
+                                            seven eight Sclemeel schlemazel hasenfeffer incorporated the
+                                            weather started getting rough the tiny ship was tossed if
+                                            not for the courage of the fearless crew the minnow would
+                                            be lost the minnow would be lost.
+                                        </p>
+                                        <a href="index2.html#" className="vol_btn">View Products<i className="flaticon-arrows-3"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
