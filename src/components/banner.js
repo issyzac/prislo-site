cc629e7bc80b45ec855a6be82e1094b31633d4cc
Isaiah <issyzac.iz@gmail.com>
2019-10-30 10:42:53 +0300

    feat: added revolution slider to the banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,59 +1,81 @@
 import React, { Component } from 'react'
+import { RevSlider, Slide, Caption } from 'react-rev-slider'
 
 class Banner extends Component {
 
+    config = {
+        delay: 9000,
+        startwidth: 1170,
+        startheight: 500,
+        hideThumbs: 10,
+        fullWidth: "on",
+        forceFullWidth: "on",
+        fullScreenAlignForce: "off",
+        navigationType: "bullet",
+        navigationArrows: "on",
+        navigationStyle: "round",
+        touchenabled: "on",
+        onHoverStop: "on",
+        navOffsetHorizontal: 0,
+        navOffsetVertical: 20,
+        shadow: 0,
+        fullWidth: "on",
+        fullScreen: "on",
+        navigationVOffset: 35
+    };
+
     render() {
         return (
-            <section className="banner_01">
+            <section className="banner2">
                 <div className="revSlider">
-                    <ul>
-                        <li data-transition="boxfade" data-slotamount="7" data-masterspeed="1000" >
-                            <div className="tp-caption sfb"
-                                data-x="left"
-                                data-y="top"
-                                data-hoffset="0"
-                                data-voffset="-80"
-                                data-speed="1000"
-                                data-start="800"
-                                data-easing="easeInOutCirc">
-                                <div className="sliderImgs">
-                                    <img src="images/slider/sl1.jpg" alt="" />
-                                </div>
-                            </div>
-                            <div className="tp-caption sfb"
-                                data-x="right"
+                    <RevSlider config={this.config}>
+                        <Slide
+                            src={process.env.PUBLIC_URL + '/images/slider/banner_two.jpg'}
+                            alt="slidebg1"
+                            data-bgfit="cover"
+                            data-bgposition="left top"
+                            data-bgrepeat="no-repeat"
+                            slideProperties={{
+                                'data-transition': "fade",
+                                'data-slotamount': "7",
+                                'data-masterspeed': "1500"
+                            }}>
+                            <Caption
+                                class="tp-caption skewfromrightshort fadeout"
+                                data-x="center"
                                 data-y="center"
-                                data-hoffset="0"
-                                data-voffset="0"
+                                data-voffset="-55"
                                 data-speed="1000"
                                 data-start="800"
-                                data-easing="easeInQuad">
+                                data-easing="easeInQuad"
+                            >
+                                <h6 className="sl2_h6 playfair">The Fashion Peak</h6>
+                            </Caption>
+                            <Caption
+                                class="tp-caption skewfromrightshort fadeout"
+                                data-x="center"
+                                data-y="center"
+                                data-speed="1000"
+                                data-start="800"
+                                data-easing="Power4.easeOut"
+                            >
                                 <h1 className="sl_h raleway">FEEL THE TRENDY FASHION</h1>
-                            </div>
-                            <div className="tp-caption fade"
-                                data-x="right"
+                            </Caption>
+                            <Caption
+                                class="tp-caption skewfromrightshort fadeout"
+                                data-x="center"
                                 data-y="center"
-                                data-hoffset="0"
-                                data-voffset="65"
+                                data-voffset="86"
                                 data-speed="1000"
                                 data-start="800"
-                                data-easing="easeInQuint">
-                                <h6 className="sl_p poppins">Get FLAT 30% Off For All New Arrivals</h6>
-                            </div>
-                            <div className="tp-caption sfb"
-                                data-x="right"
-                                data-y="center"
-                                data-hoffset="0"
-                                data-voffset="145"
-                                data-speed="1000"
-                                data-start="800"
-                                data-easing="easeOutBounce">
+                                data-easing="easeInQuad"
+                            >
                                 <div className="slbutons">
-                                    <a href="index.html#" className="vol_btn poppins">Shop Now<i className="flaticon-arrows-3"></i></a>
+                                    <a href="index2.html#" className="vol_btn poppins">VIEW COLLECTION<i className="flaticon-arrows-3"></i></a>
                                 </div>
-                            </div>
-                        </li>
-                    </ul>
+                            </Caption>
+                        </Slide>
+                    </RevSlider>
                 </div>
             </section>
         )
