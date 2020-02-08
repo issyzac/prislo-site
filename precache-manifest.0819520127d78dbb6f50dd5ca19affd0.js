98515b3e619ff3f0ad652d6341552fdeb94777a2
issyzac <issyzac.iz@gmail.com>
2020-02-08 21:52:00 +0300

    Updates

---
+++ precache-manifest.0819520127d78dbb6f50dd5ca19affd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa170927725273a57c54d6ab0af52035",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d089ffd8401c55789f7",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "edf9c58ea27aca734cbb",
+    "url": "/static/js/2.a16e1308.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a16e1308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d089ffd8401c55789f7",
+    "url": "/static/js/main.8db8bcfa.chunk.js"
+  },
+  {
+    "revision": "3326bc24a45c0519e72d",
+    "url": "/static/js/runtime-main.85d26332.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
