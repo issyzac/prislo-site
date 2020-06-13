17d866b38d7ea57110bff0018f68fe757244afc0
issyzac <issyzac.iz@gmail.com>
2020-06-13 14:45:16 +0300

    Content review and added contact details

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,22 +8,23 @@ export default function Footer() {
                     <div className="col-lg-4 col-sm-6">
                         <aside className="widget">
                             <div className="textwidget">
-                                <h1> Prislo </h1>
+                                <h3> Prislo Designs Tanzania. </h3>
                                 <div className="footerDesc">
-                                    Innovative and Creative and Eco-friendly bags designed with love!
+                                Innovative and Creative and Eco-friendly bags designed with love! Contact us for enquires, but yes, we deliver worldwide!
                                 </div>
                             </div>
                         </aside>
                     </div>
                     <div className="col-lg-4 col-sm-6 paddLeft11">
                         <aside className="widget">
-                            <h3 className="widgetTitle">Informations</h3>
+                            <h3 className="widgetTitle">Contact Us</h3>
                             <ul>
-                                <li><a href="index2.html#"> Contact Us </a></li>
+                                <li><a href="index2.html#"> nnkoprisca@gmail.com  </a></li>
+                                <li><a href="index2.html#"> +255 715 007 607  </a></li>
                             </ul>
                         </aside>
                     </div>
-                    <div className="clearfix hidden-lg hidden-md hidden-xs"></div>
+                    <div className="clearfix hidden-lg hidden-md hidden-s"></div>
                     <div className="col-lg-4 col-sm-6 noPaddingLeft">
                         <aside className="widget">
                             <h3 className="widgetTitle">Follow Us</h3>
